9d328ee86289b5314ff24b9eef1a84ecbf3a3138
slava.sh <shk.slava@gmail.com>
2014-05-20 00:30:39 +0600

    Add Yandex.Metrika

--- index.php
+++ index.php
@@ -58,5 +58,6 @@
     <footer style="text-align: center; padding-top: 20px; padding-bottom: 20px">
         Пишите: <a href="mailto:mail@ucteam.ru?Subject=Генератор рефератов">mail@ucteam.ru</a>
     </footer>
+    <!-- Yandex.Metrika counter --><script type="text/javascript">var yaParams = { page: 'ref' };</script><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter22576558 = new Ya.Metrika({id:22576558, clickmap:true, accurateTrackBounce:true,params:window.yaParams||{ }}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/22576558" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 </body>
 </html>
